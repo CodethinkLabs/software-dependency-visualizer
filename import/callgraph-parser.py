9a986dce01854ada91644cad6af8cbac6eef553a
Jim MacArthur <jim.macarthur@codethink.co.uk>
2016-05-11 17:38:36 +0100

    Fix shebang on python scripts in import and add file permissions

--- import/callgraph-parser.py
+++ import/callgraph-parser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # This is meant to scan a directory for files called 'calls.*' which
 # are in a proprietary text format showing the calls between objects.
