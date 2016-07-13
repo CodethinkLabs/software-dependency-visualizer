a47bc7bbd60d51cbbdbcfcc530ecc2ae8be63a59
Jim MacArthur <jim.macarthur@codethink.co.uk>
2016-07-13 11:54:38 +0100

    Move the example data out to another file

--- block-layout/examples/object-view-demo.ts
+++ block-layout/examples/object-view-demo.ts
@@ -1,19 +1,4 @@
-var exampleJSON = [
-    { "symbolName": "Sym1", "parent": "d3d.o", "sortIndex": 0, "_id": 0 },
-    { "symbolName": "Sym2", "parent": "d3d.o", "sortIndex": 1, "_id": 1 },
-    { "symbolName": "Sym3", "parent": "d3d.o", "sortIndex": 2, "_id": 2 },
-    { "symbolName": "Sym4", "parent": "d3d.o", "sortIndex": 2, "_id": 3 },
-    { "symbolName": "Sym1", "parent": "ttf.o", "sortIndex": 0, "_id": 4 },
-    { "symbolName": "Sym2", "parent": "ttf.o", "sortIndex": 0, "_id": 5 },
-    { "symbolName": "Sym3", "parent": "ttf.o", "sortIndex": 0, "_id": 6 },
-    { "symbolName": "Sym1", "parent": "alx.o", "sortIndex": 0, "_id": 7 },
-    { "symbolName": "Sym2", "parent": "alx.o", "sortIndex": 1, "_id": 8 },
-    { "symbolName": "Sym3", "parent": "alx.o", "sortIndex": 2, "_id": 9 },
-    { "symbolName": "Sym1", "parent": "klf.o", "sortIndex": 0, "_id": 10 },
-    { "symbolName": "Sym2", "parent": "klf.o", "sortIndex": 1, "_id": 11 },
-    { "symbolName": "Sym3", "parent": "klf.o", "sortIndex": 2, "_id": 12 },
-    { "symbolName": "Sym4", "parent": "klf.o", "sortIndex": 2, "_id": 13 },
-];
+/// <reference path="exampleData.ts" />
 
 const objectsColWidth = 400;
 const packagesColWidth = 200;
@@ -86,18 +71,6 @@ function makeCaption(node : GraphDBNode) : string
     var uriParts : string[] = node.uri.split(':');
     return uriParts[uriParts.length-1];
 }
-
-var exampleCalls : Call[] = [
-    { source: 0, target: 4 },
-    { source: 2, target: 5 },
-    { source: 7, target: 7 },
-    { source: 5, target: 10 },
-    { source: 1, target: 2 },
-    { source: 2, target: 5 },
-    { source: 12, target: 1 },
-    { source: 13, target: 13 },
-    { source: 0, target: 12 },
-];
 
 // Add the item to the set unless it's there already, and
 // return the new set. The original is also modified, unless
